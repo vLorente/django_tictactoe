82af997d9e273a755bb75d02343de38f3cf3f401
vLorente <valenlj7@gmail.com>
2024-07-29 17:15:19 +0200

    Update Api docs info

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -22,12 +22,10 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="TicTacToe",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      description="TicTacToe Game API para prueba técnica phicus",
+      contact=openapi.Contact(email="vlorentejimenez@gmail.com"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
